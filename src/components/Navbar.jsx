78ab715dfece99d09058b344503ee1ebb608124f
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-01-31 14:43:47 +0530

    removed css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='h-16 bg-[#262626] text-white flex justify-between items-center px-4 top-[18px] fixed z-10'>
+    <div className='h-[54px] bg-[#262626] text-white flex justify-between items-center px-4 top-[18px] fixed z-100 m-b-[1px] w-full'>
       <div className='flex items-center space-x-2'>
         <h3 className='text-white'>envato<span>market</span></h3>
       </div>
